25adaefb2cc602b9e441479d4e424eb85e101342
Reyner Veliz <reynerv@yahoo.com>
2022-11-05 09:02:27 -0400

    Create first component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-
 import './App.css';
+import ExpenseItem from './components/ExpenseItem';
 
 function App() {
   return (
     <div>
       <h1>Hello World!</h1>
+      <ExpenseItem />
     </div>
   );
 }
