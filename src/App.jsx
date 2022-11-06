d84f64e32b66aad43526dd1f36dcd590f89a497f
Reyner Veliz <reynerv@yahoo.com>
2022-11-06 08:12:28 -0500

    Add normal javascript logic to components

--- src/App.jsx
+++ src/App.jsx
@@ -25,10 +25,22 @@ function App() {
   ];
   return (
     <div>
-      <ExpenseItem title={expenses[0].title} date={expenses[0].date.toLocaleDateString()} amount={expenses[0].amount} />
-      <ExpenseItem title={expenses[1].title} date={expenses[1].date.toLocaleDateString()} amount={expenses[1].amount} />
-      <ExpenseItem title={expenses[2].title} date={expenses[2].date.toLocaleDateString()} amount={expenses[2].amount} />
-      <ExpenseItem title={expenses[3].title} date={expenses[3].date.toLocaleDateString()} amount={expenses[3].amount} />
+      <ExpenseItem 
+        title={expenses[0].title} 
+        date={expenses[0].date} 
+        amount={expenses[0].amount} />
+      <ExpenseItem 
+        title={expenses[1].title} 
+        date={expenses[1].date} 
+        amount={expenses[1].amount} />
+      <ExpenseItem 
+        title={expenses[2].title} 
+          date={expenses[2].date} 
+          amount={expenses[2].amount} />
+      <ExpenseItem 
+        title={expenses[3].title} 
+        date={expenses[3].date} 
+        amount={expenses[3].amount} />
     </div>
   );
 }
--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -3,7 +3,7 @@ import './ExpenseItem.css';
 const ExpenseItem = props => {
     return (
         <div className='expense-item'>
-            <div>{props.date}</div>
+            <div>{props.date.toLocaleString('en-US', {month: 'long'})}</div>
             <div className='expense-item__description'>
                 <h2>{props.title}</h2>
                 <div className='expense-item__price'>{props.amount}</div>
