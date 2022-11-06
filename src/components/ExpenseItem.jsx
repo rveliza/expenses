7fd855b92155f582b867fcd783e647bbb5d96b89
Reyner Veliz <reynerv@yahoo.com>
2022-11-05 20:40:03 -0400

    Output dynamic data

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,12 +1,17 @@
 import './ExpenseItem.css';
 
 const ExpenseItem = () => {
+    const expenseDate = new Date(2023, 3, 28);
+    // Fri Apr 28 2023 00:00:00 GMT-0400 (hora de verano oriental)
+    const expenseTitle = 'Car Insurance';
+    const expenseAmount = 294.67;
+
     return (
         <div className='expense-item'>
-            <div>March 28th 2023</div>
+            <div>{expenseDate.toLocaleDateString()}</div>
             <div className='expense-item__description'>
-                <h2>Car Insurance</h2>
-                <div className='expense-item__price'>294.67</div>
+                <h2>{expenseTitle}</h2>
+                <div className='expense-item__price'>{expenseAmount}</div>
             </div>
         </div>
     );
