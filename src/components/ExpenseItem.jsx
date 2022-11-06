59b614cfb6ac9318fe7254937dcb98ecd57926b3
Reyner Veliz <reynerv@yahoo.com>
2022-11-05 21:07:41 -0400

    Passing data via props

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,17 +1,12 @@
 import './ExpenseItem.css';
 
-const ExpenseItem = () => {
-    const expenseDate = new Date(2023, 3, 28);
-    // Fri Apr 28 2023 00:00:00 GMT-0400 (hora de verano oriental)
-    const expenseTitle = 'Car Insurance';
-    const expenseAmount = 294.67;
-
+const ExpenseItem = props => {
     return (
         <div className='expense-item'>
-            <div>{expenseDate.toLocaleDateString()}</div>
+            <div>{props.date}</div>
             <div className='expense-item__description'>
-                <h2>{expenseTitle}</h2>
-                <div className='expense-item__price'>{expenseAmount}</div>
+                <h2>{props.title}</h2>
+                <div className='expense-item__price'>{props.amount}</div>
             </div>
         </div>
     );
