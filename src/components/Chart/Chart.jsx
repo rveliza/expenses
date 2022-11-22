024863ea07893ebe2550193e089a22d8c28ce81a
Reyner Veliz <36872223+rveliza@users.noreply.github.com>
2022-11-22 16:05:44 -0600

    Add ChartBar

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,12 +1,25 @@
+import ChartBar from "./ChartBar";
+
 const Chart = props => {
     // console.log(props.dataPoints);
     const dataPointValues = props.dataPoints.map(dataPoint => dataPoint.value);
-    console.log(dataPointValues); 
+    // console.log(dataPointValues); 
     // [0, 0, 0, 0, 0, 0, 0, 94.12, 0, 0, 0, 130]
     const totalMaximum = Math.max(...dataPointValues);
-    console.log(totalMaximum);
+    // console.log(totalMaximum);
     return (
-        <h1>Hello</h1>
+        <div className="chart">
+            {
+                props.dataPoints.map((dataPoint) => (
+                    <ChartBar 
+                        key={dataPoint.label}
+                        value={dataPoint.value}
+                        maxValue={totalMaximum}
+                        label={dataPoint.label}
+                    />
+                ))
+            }
+        </div>
     );
 }
 
